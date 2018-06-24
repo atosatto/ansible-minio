363d01b976b61374f15cb8a04684f5388d034be0
Ben Kochie <superq@gmail.com>
2018-06-24 15:11:21 +0200

    Fix up tests.

--- molecule/default/tests/test_minio.py
+++ molecule/default/tests/test_minio.py
@@ -12,6 +12,18 @@ def AnsibleDefaults(Ansible):
         return yaml.load(stream)
 
 
+@pytest.mark.parametrize("dirs", [
+    "/minio-test"
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+    assert d.user == AnsibleDefaults['minio_user']
+    assert d.group == AnsibleDefaults['minio_group']
+    assert oct(d.mode) == '0750'
+
+
 @pytest.mark.parametrize('minio_bin_var', [
     'minio_server_bin',
     'minio_client_bin',
@@ -23,18 +35,6 @@ def test_minio_installed(File, AnsibleDefaults, minio_bin_var):
     assert f.user == AnsibleDefaults['minio_user']
     assert f.group == AnsibleDefaults['minio_group']
     assert oct(f.mode) == '0755'
-
-
-@pytest.mark.parametrize("dirs", [
-    "/test-minio"
-])
-def test_directories(host, dirs):
-    d = host.file(dirs)
-    assert d.is_directory
-    assert d.exists
-    assert d.user == AnsibleDefaults['minio_user']
-    assert d.group == AnsibleDefaults['minio_group']
-    assert oct(d.mode) == '0750'
 
 
 def test_minio_service(Service):
