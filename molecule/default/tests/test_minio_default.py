0bceb1d5d80a7e19cc2c8bb578c42f3b070c3775
Sébastien <s.berthier@bee-buzziness.com>
2019-07-23 17:00:01 +0200

    Fix ansible-lint error on double quote (#37)

--- molecule/default/tests/test_minio_default.py
+++ molecule/default/tests/test_minio_default.py
@@ -10,7 +10,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 @pytest.fixture()
 def AnsibleDefaults():
-    with open("../../defaults/main.yml", 'r') as stream:
+    with open('../../defaults/main.yml', 'r') as stream:
         return yaml.load(stream)
 
 
@@ -40,4 +40,4 @@ def test_minio_server_data_directories(host, AnsibleDefaults):
 
 def test_minio_server_webserver(host):
 
-    host.socket("tcp://127.0.0.1:9091").is_listening
+    host.socket('tcp://127.0.0.1:9091').is_listening
