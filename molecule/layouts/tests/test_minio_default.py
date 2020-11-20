0c55454439c105d95e59a5ddee6f9f6823125730
Gregor <gregor.jahn@wire.com>
2020-11-20 01:27:05 +0100

    Get back to upstream
    This change set aims get close to upstream again, before completely
    diverging from the fork (see FUTUREWORK). This would ease a possible
    rejoin in the future.
    
    As a result, various changes revert recent work done in this fork. The
    reasoning, in part, is not known anymore.
    
    It also introduces some breaking changes. Namely, the interface for
    multiple layouts. The layout name is derived from the respective key
    in 'minio_layouts'. Additionally, the 'layouts' variable has been
    renamed to follow a common role pattern, that is to prefix each one.
    
    Instead of using the default test scenario, this introduces a dedicated
    scenario to test the multi-layout setup. It is very similar to the
    'default' scenario, but applies the role twice just with different layouts
    and on less instances.
    
    Please note, that atm the role only supports Ansible version >= 2.7, < 2.8
    
    FUTUREWORK:
    
    * upgrade to python3, ansible 2.10
    * testing
      * move von travis to github actions
      * upgrade to Molecule 3
    * refactor the the layouts idea, so that there is a default value for
      'minio_layout' instead of joining global attributes (e.g. 'minio_server_datadirs')
      with the corresponding ones defined for a specific layout

---
+++ molecule/layouts/tests/test_minio_default.py
@@ -0,0 +1,54 @@
+import os
+import yaml
+import pytest
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.fixture()
+def AnsibleDefaults():
+    with open('../../defaults/main.yml', 'r') as stream:
+        return yaml.load(stream)
+
+@pytest.fixture()
+def AnsiblePlaybook():
+    with open('./playbook.yml', 'r') as stream:
+        return yaml.load(stream)
+
+
+@pytest.mark.parametrize('minio_bin_var', [
+    'minio_server_bin',
+    'minio_client_bin',
+])
+def test_minio_installed(host, AnsibleDefaults, minio_bin_var):
+
+    f = host.file(AnsibleDefaults[minio_bin_var])
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert oct(f.mode) == '0755'
+
+
+def test_minio_server_data_directory(host, AnsibleDefaults, AnsiblePlaybook):
+
+    playbpook = AnsiblePlaybook[0]
+    for role in playbpook['roles']:
+        layoutName = role['vars']['minio_layout']
+        datadir = "/var/lib/minio-{}".format(layoutName)
+        d = host.file(datadir)
+        assert d.is_directory
+        assert d.exists
+        assert d.user == AnsibleDefaults['minio_user']
+        assert d.group == AnsibleDefaults['minio_group']
+        assert oct(d.mode) == '0750'
+
+
+def test_minio_server_webservers(host, AnsibleDefaults):
+
+    for layoutName in AnsibleDefaults['minio_layouts'].keys():
+        server_addr = AnsibleDefaults['minio_layouts'][layoutName]['server_addr']
+        addr = "tcp://127.0.0.1{}".format(server_addr)
+        host.socket(addr).is_listening
